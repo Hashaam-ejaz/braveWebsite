a21c50d5b2cf3928a34c4dff78e03e50675f6361
HashaamBinEjaz <139843321+HashaamBinEjaz@users.noreply.github.com>
2024-07-31 20:25:36 +0500

    Header completed and working

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
 const Header = () => {
-  return <div>Headerrr</div>;
+  return (
+    <div className="flex w-full h-full font-garamond py-8 text-3xl mx-12 ">
+      <Link className="flex mr-4" href={"/"}>
+        Brave
+      </Link>
+      <div className="flex w-[2px] h-8 bg-black mr-4" />
+      <Link className="flex mr-4" href={"/approach"}>
+        Our Approach
+      </Link>
+      <div className="flex w-[2px] h-8 bg-black mr-4" />
+      <Link className="flex mr-4" href={"/family"}>
+        {" "}
+        Our Family
+      </Link>
+      <div className="flex w-[2px] h-8 bg-black mr-4" />
+      <Link className="flex mr-4" href={"/moments"}>
+        {" "}
+        Our Moments
+      </Link>
+    </div>
+  );
 };
 
 export default Header;
