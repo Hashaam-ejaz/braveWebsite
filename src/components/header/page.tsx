e52d5d042dfebe646e20e60b3496d0f81718f62f
Kame <56468265+Areeb5146@users.noreply.github.com>
2024-07-31 23:37:13 -0700

    initial layout of home

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,22 +1,20 @@
 import Link from "next/link";
 const Header = () => {
   return (
-    <div className="flex w-full h-full font-garamond py-8 text-3xl mx-12 ">
-      <Link className="flex mr-4" href={"/"}>
+    <div className="absolute top-0 left-0  flex font-garamond py-8 text-3xl mx-12 z-20">
+      <Link className="mr-4" href={"/"}>
         Brave
       </Link>
-      <div className="flex w-[2px] h-8 bg-black mr-4" />
-      <Link className="flex mr-4" href={"/approach"}>
+      <div className="w-[2px] h-8 bg-black mr-4" />
+      <Link className="mr-4" href={"/approach"}>
         Our Approach
       </Link>
-      <div className="flex w-[2px] h-8 bg-black mr-4" />
-      <Link className="flex mr-4" href={"/family"}>
-        {" "}
+      <div className="w-[2px] h-8 bg-black mr-4" />
+      <Link className="mr-4" href={"/family"}>
         Our Family
       </Link>
-      <div className="flex w-[2px] h-8 bg-black mr-4" />
-      <Link className="flex mr-4" href={"/moments"}>
-        {" "}
+      <div className="w-[2px] h-8 bg-black mr-4" />
+      <Link className="mr-4" href={"/moments"}>
         Our Moments
       </Link>
     </div>
