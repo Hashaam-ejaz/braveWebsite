6368952390056346f769258d3b18a41b8ec01b16
Hashaam-Ejaz <hashaamejaz@gmail.com>
2025-01-10 14:24:50 +0500

    header done.

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,10 +1,32 @@
+"use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import braveLogo from "../../../public/images/braveLogo.svg";
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 180); // Adjust the scroll value as needed
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="absolute top-0 z-20 flex justify-around items-center w-full text-white font-garamond mt-[20px] lg:mt-[26px] md:text-[24px] text-[13px] lg:text-[32px]">
+    <div
+      className={`mt-[-6rem] pt-3 sticky top-0 z-20 flex justify-around items-center md:text-[24px] text-[13px] lg:text-[32px] pb-2 transition-all duration-200 ease-in-out ${
+        scrolled
+          ? "bg-[rgba(255,255,255,0.7)] backdrop-blur-[6px] text-black"
+          : "bg-transparent text-white"
+      }`}
+    >
       <Link href={"/"}>
         <Image
           src={braveLogo}
@@ -14,21 +36,21 @@ const Header = () => {
         />
       </Link>
 
-      <div className="w-[1px] h-6 lg:h-12 bg-white " />
+      <div className="w-[1px] h-6 lg:h-12 bg-white" />
 
       <Link href={"/"}>
         <span className="font-bold">BRAVE</span>
       </Link>
 
-      <div className="w-[1px] h-6 lg:h-12 bg-white " />
+      <div className="w-[1px] h-6 lg:h-12 bg-white" />
 
       <Link href={"/approach"}>Our Approach</Link>
 
-      <div className="w-[1px] h-6 lg:h-12 bg-white " />
+      <div className="w-[1px] h-6 lg:h-12 bg-white" />
 
       <Link href={"/family"}>Our Family</Link>
 
-      <div className="w-[1px] h-6 lg:h-12 bg-white " />
+      <div className="w-[1px] h-6 lg:h-12 bg-white" />
 
       <Link href={"/moments"}>Our Moments</Link>
     </div>
