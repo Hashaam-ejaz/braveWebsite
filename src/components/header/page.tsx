3f0627bb3d3fdfc1ee4292ed53c0c5ea2a1801cd
Hashaam-Ejaz <hashaamejaz@gmail.com>
2025-01-20 13:39:34 +0500

    header logo sized reduced and now indicates current page

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,36 +1,45 @@
+"use client"; // Required since `usePathname` is a client-side hook in Next.js
+
 import Link from "next/link";
 import Image from "next/image";
 import braveLogo from "../../../public/images/braveLogo.svg";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname(); // Get the current pathname
+
+  // Helper function to determine active link styles
+  const getLinkClass = (path: string) => {
+    return pathname === path ? "font-extrabold" : ""; // Add 'font-extrabold' if the link matches the pathname
+  };
+
   return (
     <div className="absolute top-0 z-20 flex justify-around items-center w-full text-white mt-[20px] lg:mt-[26px] md:text-[24px] text-[13px] lg:text-[32px]">
       <Link href={"/"}>
-        <Image
-          src={braveLogo}
-          alt="Brave Logo"
-          width={50} // Adjust the width
-          height={50} // Adjust the height
-        />
+        <Image src={braveLogo} alt="Brave Logo" width={25} height={25} />
       </Link>
-
-      {/* <div className="w-[1px] h-6 lg:h-12 bg-white " /> */}
 
       <Link href={"/"}>
-        <span className="font-bold">BRAVE</span>
+        <span className={getLinkClass("/")}>BRAVE</span>
       </Link>
 
       <div className="w-[1px] h-6 lg:h-12 bg-white " />
 
-      <Link href={"/approach"}>Our Approach</Link>
+      <Link href={"/approach"}>
+        <span className={getLinkClass("/approach")}>Our Approach</span>
+      </Link>
 
       <div className="w-[1px] h-6 lg:h-12 bg-white " />
 
-      <Link href={"/family"}>Our Family</Link>
+      <Link href={"/family"}>
+        <span className={getLinkClass("/family")}>Our Family</span>
+      </Link>
 
       <div className="w-[1px] h-6 lg:h-12 bg-white " />
 
-      <Link href={"/moments"}>Our Moments</Link>
+      <Link href={"/moments"}>
+        <span className={getLinkClass("/moments")}>Our Moments</span>
+      </Link>
     </div>
   );
 };
